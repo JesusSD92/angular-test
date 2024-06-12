ec7590172f8704a028e47d28af603379433e2d43
Jesus <jesus.sd18@gmail.com>
2024-06-12 11:30:51 +0200

    Modularizar cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent],
   templateUrl: './cart.component.html'
 })
 export class CartComponent implements OnInit {
